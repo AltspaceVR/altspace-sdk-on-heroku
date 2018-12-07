79df8fb431643f92a95029fc2403642e34343a7d
Jimmy Zhang <jimmy.zhang@microsoft.com>
2018-12-07 12:59:16 -0800

    basic example working locally

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ConsoleLogger, WebHost } from '@microsoft/mixed-reality-extension-sdk';
+import { resolve as resolvePath } from 'path';
+import HelloWorld from './app';
+
+process.on('uncaughtException', (err) => console.log('uncaughtException', err));
+process.on('unhandledRejection', (reason) => console.log('unhandledRejection', reason));
+
+const logger = new ConsoleLogger();
+logger.disable('debug', 'success');
+
+ // Start listening for connections, and serve static files
+const server = new WebHost({
+   baseDir: resolvePath(__dirname, '../public'),
+   baseUrl: "http://10.0.1.89:3901",
+   logger
+});
+
+// Handle new application sessions
+server.adapter.onConnection(context => new HelloWorld(context, server.baseUrl));
